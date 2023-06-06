c5caa4a8085fb34d8db77573fd21f4e277a9ea8d
noora20FH <noora20fairy@gmail.com>
2023-06-06 18:48:32 +0700

    retrieve student data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Students from "./components/Students";
+import Manage from "./components/Manage";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+         <Route exact path="/" element={<Home/>} />
+         <Route path="/students" element={<Students/>} />
+         <Route path="/manage" element={<Manage/>} />
+       </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
